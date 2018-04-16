15604bb891d4d7a65f239fefb26876c73ca14a8d
amit4767 <amitmeme@gmail.com>
2018-04-16 23:57:28 +0530

    update comments

--- src/main/java/com/db/awmd/challenge/dao/TransferMoneyImpl.java
+++ src/main/java/com/db/awmd/challenge/dao/TransferMoneyImpl.java
@@ -15,7 +15,9 @@ public class TransferMoneyImpl implements TransferMoney {
 
     @Override
     public  synchronized void transferMoney(Account accountfrom, Account accountto, BigDecimal transferAmount){
-
+         //TODO : Replace by Actual DB call in future.    
+        // We need to maintain ACID here . Currently i have tried revert the collection if unable to update
+        // any acccount.
         try {
             accountsRepositoryInMemory.updateAccount(accountfrom.debitMoney(transferAmount));
             accountsRepositoryInMemory.updateAccount(accountto.creditMoney(transferAmount));
